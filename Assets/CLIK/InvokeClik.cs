c394724ce8e06b7bdd9c4caa97ec3449b856ed5c
ofekabo <76842189+ofekabo@users.noreply.github.com>
2021-07-01 11:58:21 +0300

    clik

---
+++ Assets/CLIK/InvokeClik.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tabtale.TTPlugins;
+using UnityEngine;
+
+public class InvokeClik : MonoBehaviour
+{
+	private void Awake()
+
+	{
+		TTPCore.Setup();
+	}
+}
---
+++ Assets/Example/GetBuildVersion.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GetBuildVersion : MonoBehaviour
+{
+    [ContextMenu("Get Build")]
+    void GetBuildV()
+    {
+        Debug.Log(Application.version);
+    }
+}
